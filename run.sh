0d465cb553f0d82f939eaa4e6e2af65a0818ad22
Jakub Samek <jaksamek@gmail.com>
2020-05-18 21:43:25 +0200

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 docker stop tradfri-cli
-docker run -dit --rm --name tradfri-cli tradfri-cli
+docker run -dit --rm \
+  --name tradfri-cli \
+  -e TRADFRIKEY=6KfxeJCySh58YEBg \
+  -e TRADFRIIP=192.168.0.14 \
+  -e INFLUXDBIP=192.168.0.20 \
+  tradfri-cli
